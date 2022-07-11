d89b8ab079fb82f85411a641035917e816a0b02b
Claire Q <claireq620@gmail.com>
2022-07-11 08:11:12 -0400

    Let user click away from mobile nav to close

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -8,15 +8,19 @@ import { Link } from 'react-router-dom';
 
 export const Layout = () => {
     const [sideMenuOpen, setSideMenuOpen] = useState(false);
+
     return (
         <div>
-            <div>
-                <Navbar sideMenuOpen={sideMenuOpen} setSideMenuOpen={setSideMenuOpen} />
+            <div onClick={()=>{if (sideMenuOpen) setSideMenuOpen(false)}}>
+                <div className={`bg-black w-[100%] h-[100%] z-40 fixed ${sideMenuOpen ? 'opacity-30 lg:hidden' : 'hidden'}`}/>
+                <div>
+                    <Navbar sideMenuOpen={sideMenuOpen} setSideMenuOpen={setSideMenuOpen} />
+                </div>
+                <div className='pt-14'>
+                    <Outlet/>
+                </div>
             </div>
-            <div className='pt-14'>
-                <Outlet/>
-            </div>
-            <nav className={'transform fixed top-0 left-0 bg-gray-700 w-64 h-full overflow-auto ease-in-out transition-all duration-300 z-30 text-gray-100 drop-shadow-lg lg:hidden ' + (sideMenuOpen ? 'translate-x-0' : '-translate-x-full')} >
+            <nav className={`transform fixed top-0 left-0 bg-gray-700 w-72 h-full overflow-auto ease-in-out transition-all duration-300 z-50 text-gray-100 drop-shadow-lg lg:hidden ${sideMenuOpen ? 'translate-x-0' : '-translate-x-full'}`} >
                 <div className='flex flex-col'>
                     <div>
                         <FontAwesomeIcon icon={faXmark} size='xl' className='hover:cursor-pointer float-right mr-5 mt-3' onClick={()=>setSideMenuOpen(false)}/>
