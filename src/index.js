44adaa7267a3cbf8176afe8b2232a7d27c13c017
clairox <claireq620@gmail.com>
2022-12-20 11:30:43 -0500

    Fix broken imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../assets/index.css';
+import './assets/index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ProvideAuth } from './context/authContext';
