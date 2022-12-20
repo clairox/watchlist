0800e3639147a3e3610d7d6b7448363c45441c61
clairox <claireq620@gmail.com>
2022-12-20 11:17:20 -0500

    Fix broken includes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useAuth } from '../context/authContext';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { WatchlistPreviewSlider } from './WatchlistPreviewSlider';
+import { WatchlistPreviewSlider } from '../components/WatchlistPreviewSlider';
 import { Watchlist } from '../../types';
 import { Helmet } from 'react-helmet';
 
