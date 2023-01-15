e2d5f801714a9abb3695f6f8beac562cf6ee839f
Claire Q <claireq620@gmail.com>
2023-01-15 16:35:56 -0500

    Remvove unnecessary routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,22 @@ import { HomePage } from './pages/HomePage';
 import { PrivateRoute } from './components/PrivateRoute';
 import { SignupPage } from './pages/SignupPage';
 import { Layout } from './components/Layout';
-import { ListsPage } from './pages/ListsPage';
-import { FavoritesPage } from './pages/FavoritesPage';
 import { WatchlistPage } from './pages/WatchlistPage';
 import { ErrorPage } from './pages/ErrorPage';
+import { SettingsPage } from './pages/SettingsPage';
 
-//TODO: write a README.md
-//TODO: go to 404 page if url leads nowhere
 function App() {
 	return (
 		<div className="App min-h-screen bg-gray-800 pb-20">
 			<Routes>
 				<Route element={<Layout />}>
 					<Route index element={<HomePage />} />
-					<Route path="/lists" element={<ListsPage />} />
 					<Route path="/lists/:id" element={<WatchlistPage />} />
-					<Route path="/favorites" element={<FavoritesPage />} />
+					<Route element={<PrivateRoute />}>
+						<Route path="/settings" element={<SettingsPage />} />
+					</Route>
 					<Route path="*" element={<ErrorPage code={404} message={'Page Not Found'} />} />
 				</Route>
-				<Route element={<PrivateRoute />}></Route>
 				<Route path="/login" element={<LoginPage />} />
 				<Route path="/signup" element={<SignupPage />} />
 				<Route path="/400" element={<ErrorPage code={400} message={'Bad Request'} />} />
